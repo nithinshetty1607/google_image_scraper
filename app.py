6b3c874e50dd682c454aab68a25fc722ed82c4da
R Shetty <nithin.r.shetty@accenture.com>
2021-05-27 22:33:36 +0530

    image scrapper app

--- app.py
+++ app.py
@@ -56,11 +56,7 @@ def searchImage():
     
     header={
         'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive',
+   
             }
     lst_images=imagescrapperutil.downloadImages(search_term,header)
     print(lst_images)
